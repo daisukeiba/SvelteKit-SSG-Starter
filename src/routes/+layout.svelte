7f7f56d170d89c47396d4e69e46bb5ea9a22785f
daisukeiba <iba018@gmail.com>
2023-09-21 12:07:40 +0900

    Modify: $app/environment から dev・.envファイルの値を反映・プレビューのportを3000に変更

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import { dev } from '$app/environment';
   import { page } from '$app/stores';
   import TheNav from '$lib/TheNav.svelte';
 
+  console.log({dev}); // Boolean devサーバーが使われるか
   export let data;
 
   $: pathname = $page.url.pathname;
